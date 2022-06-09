1c91e243650e4f4b40f03ac13ab7ee79f7dcd27f
Lesley-Nicole <lesley-nicole@protonmail.com>
2022-06-09 18:42:37 -0500

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -209,7 +209,7 @@ const renderButton = () => {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="./cryptodevs.svg" />
+          <img className={styles.image} src="./cryptodevs.svg" width="50%"/>
         </div>
       </div>
 
