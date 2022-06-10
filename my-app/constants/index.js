17e17e244047bfb7ec3ade1054d55a2f4ced5280
Lesley-Nicole <lesley-nicole@protonmail.com>
2022-06-09 21:08:07 -0500

    Update index.js

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,2 +1,66 @@
-export const abi = "./Whitelist.json";
+export const abi = "[
+	{
+		"inputs": [
+			{
+				"internalType": "uint8",
+				"name": "_maxWhitelistedAddresses",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "addAddressToWhitelist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxWhitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "numAddressesWhitelisted",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "whitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]";
 export const WHITELIST_CONTRACT_ADDRESS = "0x311305C99FEA2C19faF5Cb039C3416327F90F4C1";
